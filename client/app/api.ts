5a5fb2890909f0b5f2efe6ecb3153fa7cfd85004
Uttam Gohil <120741895+ItsJupiter000@users.noreply.github.com>
2026-02-17 11:34:48 +0530

    Update API base URL to use environment variable

--- client/app/api.ts
+++ client/app/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://outreacher-no1k.onrender.com' || 'http://localhost:5000/api', // Update with your backend URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
